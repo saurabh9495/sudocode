0d6b0de443c8dc9a0b439b128660c7a6e8cb9843
saurabh9495 <saurabhkumar4829@gmail.com>
2020-05-29 05:21:46 +0530

    Coding Commits made on 29th May 2020 Must Do Coding Questions GeekForGeeks.

--- Arrays/Count_the_triplets.cpp
+++ Arrays/Count_the_triplets.cpp
@@ -22,15 +22,19 @@ int main()
         int ctr = 0;
         for (int k = 0; k < v.size() - 2; k++)
         {
-            for (int l = k; l < v.size() - 2; l++)
+            for (int l = k + 1; l < v.size() - 1; l++)
             {
-                if (v[k] + v[k + 1] == v[l + 2])
+                for (int m = l + 1; m < v.size(); m++)
                 {
-                    ctr++;
-                }
-                else if (v[k] + v[k + 1] > v[l + 2])
-                {
-                    break;
+                    if (v[k] + v[l] == v[m])
+                    {
+                        ctr++;
+                        break;
+                    }
+                    else if (v[k] + v[l] < v[m])
+                    {
+                        break;
+                    }
                 }
             }
         }
@@ -45,3 +49,18 @@ int main()
     }
     return 0;
 }
+
+// 1
+// 7
+// 3 4 7 6 2 9 10
+
+//2 3 4 6 7 9 10
+//0 1 2 3 4 5 6
+
+// k=0 l=1 m=2 5!=4
+// k=0 l=1 m=3 5!=6
+// k=0 l=2 m=3 ctr=1
+// k=0 l=3 m=4 8!=7
+// k=0 l=3 m=5 8!=9
+// k=0 l=4 m=5 ctr=2
+// k=0 l=5 m=6 11!=10
