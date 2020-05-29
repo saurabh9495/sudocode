7e2beb2aa800b6a81652799f1dcb008e04523184
saurabh9495 <saurabhkumar4829@gmail.com>
2020-05-30 02:04:51 +0530

    Coding Commits made on 29th May 2020 Must Do Coding Questions GeekForGeeks.

--- Arrays/Kadane's_Algorithm.cpp
+++ Arrays/Kadane's_Algorithm.cpp
@@ -1,37 +1,32 @@
 #include <iostream>
 #include <vector>
-#include <set>
+#include <algorithm>
 using namespace std;
 typedef long long int lli;
 int main()
 {
-    int testcases;
+    lli testcases;
     cin >> testcases;
     for (lli i = 0; i < testcases; i++)
     {
         lli noe, val;
         vector<lli> v;
+        cin >> noe;
         for (lli j = 0; j < noe; j++)
         {
             cin >> val;
             v.push_back(val);
         }
-        lli max_far = INT8_MIN, max_here = 0;
-        for (lli k = 0; k < v.size(); k++)
+        int max_far = INT8_MIN, max_here = 0;
+        for (int k = 0; k < v.size(); k++)
         {
             max_here = max_here + v[k];
-            if (max_far < max_here){
+            if (max_far < max_here)
                 max_far = max_here;
-            }
-            if (max_here < 0){
+            if (max_here < 0)
                 max_here = 0;
-            }
         }
         cout << max_far << endl;
     }
     return 0;
 }
-
-// 1
-// 5
-// 1 2 3 -2 5
--- Arrays/testcases_noe_vector_template.cpp
+++ Arrays/testcases_noe_vector_template.cpp
@@ -1,23 +1,23 @@
 #include <iostream>
 #include <vector>
-#include <set>
+#include <algorithm>
 using namespace std;
 typedef long long int lli;
 int main()
 {
-    int testcases;
+    lli testcases;
     cin >> testcases;
     for (lli i = 0; i < testcases; i++)
     {
-        lli noe,val;
-        cin >> noe;
+        lli noe, val;
         vector<lli> v;
+        cin >> noe;
         for (lli j = 0; j < noe; j++)
         {
             cin >> val;
             v.push_back(val);
         }
-        
+        //code
     }
     return 0;
 }
