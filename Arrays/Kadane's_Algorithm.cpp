857e568e0de0dfe3bc9733cc4dccbc2614960ed5
saurabh9495 <saurabhkumar4829@gmail.com>
2020-05-29 16:58:59 +0530

    Coding Commits made on 29th May 2020 Must Do Coding Questions GeekForGeeks.

---
+++ Arrays/Kadane's_Algorithm.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <set>
+using namespace std;
+typedef long long int lli;
+int main()
+{
+    int testcases;
+    cin >> testcases;
+    for (lli i = 0; i < testcases; i++)
+    {
+        lli noe, val;
+        vector<lli> v;
+        for (lli j = 0; j < noe; j++)
+        {
+            cin >> val;
+            v.push_back(val);
+        }
+        lli max_far = INT8_MIN, max_here = 0;
+        for (lli k = 0; k < v.size(); k++)
+        {
+            max_here = max_here + v[k];
+            if (max_far < max_here){
+                max_far = max_here;
+            }
+            if (max_here < 0){
+                max_here = 0;
+            }
+        }
+        cout << max_far << endl;
+    }
+    return 0;
+}
---
+++ Arrays/testcases_noe_vector_template.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <set>
+using namespace std;
+typedef long long int lli;
+int main()
+{
+    int testcases;
+    cin >> testcases;
+    for (lli i = 0; i < testcases; i++)
+    {
+        lli noe,val;
+        vector<lli> v;
+        for (lli j = 0; j < noe; j++)
+        {
+            cin >> val;
+            v.push_back(val);
+        }
+        
+    }
+    return 0;
+}
